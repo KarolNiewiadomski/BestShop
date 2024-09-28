417f140d8e4fec33aa00543b33f99b9e6c4f7e72
KarolNiewiadomski <karolniewiadomski@outlook.com>
2024-09-28 15:56:14 +0200

    added 2 functions updateTotalPrice and hideElements in the js file

--- 01_Best_shop/main.js
+++ 01_Best_shop/main.js
@@ -4,6 +4,7 @@ const inputPackage = document.querySelector("#package");
 const dropDown = document.querySelector(".select_dropdown");
 const accountingCheckbox = document.querySelector("#accounting");
 const terminalCheckbox = document.querySelector("#terminal");
+const totalPrice = document.querySelector(".total_price");
 
 const updateTotalPrice = () => {
   let totalSum = 0;
@@ -11,4 +12,10 @@ const updateTotalPrice = () => {
     totalSum += Number;
     item.innerText.replace("$", "");
   });
+  totalPrice.innerText = "$" + totalSum;
+};
+
+const hideElements = () => {
+  const listItem = document.querySelectorAll(".item_calc");
+  listItem.forEach((item) => (item.style.display = "none"));
 };
